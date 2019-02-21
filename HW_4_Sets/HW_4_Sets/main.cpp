78f9c6b062ed9682578428e4c085af1fde4a8a47
codyaverett <codyaverett@gmail.com>
2019-02-21 00:44:43 -0600

    Start HW 4

---
+++ HW_4_Sets/HW_4_Sets/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * Cody Averett
+ * HW 4 Sets
+ * 03/06/2019
+ */
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
---
+++ HW_4_Sets/HW_4_Sets/tree.h
@@ -0,0 +1,25 @@
+#ifndef TREE_H
+#define TREE_H
+
+namespace nwacc
+{
+
+	template <typename T>
+	class tree
+	{
+	private:
+
+
+		struct node
+		{
+			T * data;
+			node * left;
+			node * right;
+
+
+		};
+	public:
+	};
+}
+
+#endif
